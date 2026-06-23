4c67809f573d584d5db9330abdc7a4020158805f
ishant321 <ishantnayakk@gmail.com>
2026-06-23 20:07:27 +0530

    feat: add refresh token api

--- backend/src/main/java/com/dsarecur/backend/dto/AuthResponse.java
+++ backend/src/main/java/com/dsarecur/backend/dto/AuthResponse.java
@@ -5,11 +5,26 @@ import org.springframework.http.HttpStatus;
 public class AuthResponse {
     private String token;
 
-    public AuthResponse(String token) {
+    private String refreshToken;
+
+    public AuthResponse(String token, String refreshToken) {
         this.token = token;
+        this.refreshToken = refreshToken;
     }
 
     public String getToken() {
         return token;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
     }
 }
