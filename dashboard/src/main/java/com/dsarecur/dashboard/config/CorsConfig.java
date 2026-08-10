5931f5f91fd00502a0987b028ae508cf1021f57b
ishant321 <ishantnayakk@gmail.com>
2026-08-10 19:35:36 +0530

    fix(app): Fix cors

--- dashboard/src/main/java/com/dsarecur/dashboard/config/CorsConfig.java
+++ dashboard/src/main/java/com/dsarecur/dashboard/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "https://dsa-recur-git-main-ishant321s-projects.vercel.app")
+                        .allowedOrigins("http://localhost:3000", "https://dsa-recur-git-main-ishant321s-projects.vercel.app", "https://dsa-recur.ee-app.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
